c31bcf2e4107a3ba93ce3cfaeb9c7c5d055d733d
justintemps <jesmith627@gmail.com>
2019-10-28 23:09:56 +0100

    Update

--- src/js/utils.js
+++ src/js/utils.js
@@ -71,23 +71,3 @@ export function initCamera(initialPosition) {
 
   return camera;
 }
-
-/**
- * Initialize trackball controls to control the scene
- *
- * @param {THREE.Camera} camera
- * @param {THREE.Renderer} renderer
- */
-export function initTrackballControls(camera, renderer) {
-  const trackballControls = new TrackballControls(camera, renderer.domElement);
-  trackballControls.rotateSpeed = 1.0;
-  trackballControls.zoomSpeed = 1.2;
-  trackballControls.panSpeed = 0.8;
-  trackballControls.noZoom = false;
-  trackballControls.noPan = false;
-  trackballControls.staticMoving = true;
-  trackballControls.dynamicDampingFactor = 0.3;
-  trackballControls.keys = [65, 83, 68];
-
-  return trackballControls;
-}
